ff1c5c4b74349070839c025d1f4290299c4280b6
Daniel Huenul <danielhuenul8@gmail.com>
2022-10-24 23:23:38 -0300

    ADD: Aggregated simple token

--- src/controller/login.controller.ts
+++ src/controller/login.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 
 const client_id = '3a435c57f69346d3878f55e5bf4e049f';
 const client_secret = '36cdde2fa393451bbef5b520e11a891a';
-const redirect_uri = 'http://localhost:8888/callback';
+const redirect_uri = 'http://127.0.0.1:5173/maker';
 const stateKey = 'spotify_auth_state';
 
 const generateRandomString = function(length) {
@@ -18,12 +18,13 @@ const generateRandomString = function(length) {
 export default (req: Request, res: Response) =>{
   const state = generateRandomString(16);
   const scope = 'user-read-private user-read-email playlist-modify-private playlist-modify-public';
-  const loginUrl = `https://accounts.spotify.com/authorize` +
+  const url = `https://accounts.spotify.com/authorize` +
     `?response_type=code` +
     `&client_id=${client_id}` +
     `&scope=${scope}` +
     `&redirect_uri=${redirect_uri}` +
     `&state=${state}`
 
-  return res.status(200).json({ url: loginUrl })
+  const simpleToken = new Buffer(client_id + ':' + client_secret).toString('base64')
+  return res.status(200).json({ url, simpleToken })
 }
